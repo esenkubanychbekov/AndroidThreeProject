a71aa275a15420cfff041d0020ecfe9c0771a7e5
esen <esen@esen.kubanychbekov>
2019-10-13 01:10:02 +0600

    Add notification

--- app/src/main/java/com/example/introscreen/ui/fragments/MapFragment.java
+++ app/src/main/java/com/example/introscreen/ui/fragments/MapFragment.java
@@ -2,13 +2,18 @@ package com.example.introscreen.ui.fragments;
 
 
 
+import android.app.Notification;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import com.example.introscreen.R;
 import com.mapbox.mapboxsdk.Mapbox;
@@ -19,6 +24,8 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 
+import static com.example.introscreen.App.CHANEL_1;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,10 +33,12 @@ import com.mapbox.mapboxsdk.maps.Style;
 public class MapFragment extends Fragment {
 
     private MapView mapView;
+    Button button;
 
     public MapFragment() {
 
     }
+
 
     @Nullable
     @Override
@@ -39,6 +48,7 @@ public class MapFragment extends Fragment {
         View view =   inflater.inflate(R.layout.fragment_map, container, false);
         mapView = view.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+        button = view.findViewById(R.id.btnNotification);
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull final MapboxMap mapboxMap) {
@@ -56,10 +66,30 @@ public class MapFragment extends Fragment {
                 });
             }
         });
-
-
+        createNotification();
         return view;
     }
+
+    private void createNotification(){
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Notification notification = new NotificationCompat.Builder(getContext(),CHANEL_1)
+                        .setSmallIcon(R.drawable.ic_one)
+                        .setContentTitle("My notification")
+                        .setContentText("Much longer text that cannot fit one line...")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                        .build();
+                NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getContext());
+                notificationManagerCompat.notify(1,notification);
+            }
+        });
+
+    }
+
+
 
     @Override
     public void onStart() {
